197e4fef35ae1bf3b4718c3e64bb35bb302a6a90
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2020-08-07 18:59:24 +0200

    clena Decomposable

--- decomposable.rb
+++ decomposable.rb
@@ -1,20 +1,36 @@
+
 class Decomposable
-  attr_accessor :number, :combinations
+  attr_accessor :number, :components
 
   def initialize(number)
-    @number = number
-    @combinations =[]
+    @number     = number
+    @components = [[number]]
   end
 
   def to_s
     @number
   end
 
-  def smallest_component
+  def product(decomposeble)
+    components.product(decomposeble.components).map { |p| p.flatten }
+  end
+
+  def components
+    return @components unless can_be_decomposed?
+
+    new_components = component_numbers.to_a.map { |num| product_with_pair(num) }
+    new_components.each { |v| @components.concat v }
+    @components = @components.map { |r| r.sort }.sort.uniq
+    @components.select { |a| a.uniq == a }
+  end
+
+  private
+
+  def smallest_number
     number / 2 + 1
   end
 
-  def largest_component
+  def largest_number
     number - 1
   end
 
@@ -22,27 +38,13 @@ class Decomposable
     ![1, 2].include?(number)
   end
 
-  def combine(decomposeble)
-    compositions.product(decomposeble.compositions).map { |p| p.flatten }
+  def component_numbers
+    (smallest_number..largest_number)
   end
 
-  def compositions
-    return [number] unless can_be_decomposed?
-
-    combinations = [] << [number]
-    (smallest_component..largest_component).to_a.map do |a|
-      b = number - a
-      a,b = Decomposable.new(a), Decomposable.new(b)
-      combinations.concat(a.combine(b))
-    end
-    combinations = combinations.map { |r| r.sort }.sort.uniq
-    combinations = combinations.select { |a| a.uniq == a }
-    return combinations
+  def product_with_pair(a)
+    b    = number - a
+    a, b = Decomposable.new(a), Decomposable.new(b)
+    (a.product(b))
   end
 end
-
-# comositions = Decomposable.new(10).compositions
-# comositions.each { |dc| puts "#{dc}" }
-# flat_comp = comositions.flatten
-# flat_comp.uniq.sort.each { |n| puts "#{n}: #{flat_comp.count(n)}/#{comositions.size}" }
-
--- spec/decomposable_spec.rb
+++ spec/decomposable_spec.rb
@@ -2,30 +2,30 @@ require './decomposable'
 
 RSpec.describe Decomposable do
   it "test 1 compositions" do
-    expected = [1]
-    expect(Decomposable.new(1).compositions).to eq(expected)
+    expected = [[1]]
+    expect(Decomposable.new(1).components).to eq(expected)
   end
 
   it "test 2 compositions" do
-        expected = [2]
-    expect(Decomposable.new(2).compositions).to eq(expected)
+        expected = [[2]]
+    expect(Decomposable.new(2).components).to eq(expected)
   end
 
   it "test 3 compositions" do
     expected = [[1, 2], [3]]
-    expect(Decomposable.new(3).compositions).to eq(expected)
+    expect(Decomposable.new(3).components).to eq(expected)
   end
 
   it "test 6 compositions" do
         expected =[[1, 2, 3], [1, 5], [2, 4], [6]]
 
-    expect(Decomposable.new(6).compositions).to eq(expected)
+    expect(Decomposable.new(6).components).to eq(expected)
   end
 
   it "test 10 compositions" do
     expected = [[1, 2, 3, 4], [1, 2, 7], [1, 3, 6],
                 [1, 4, 5], [1, 9], [2, 3, 5], [2, 8],
                 [3, 7], [4, 6], [10]]
-    expect(Decomposable.new(10).compositions).to eq(expected)
+    expect(Decomposable.new(10).components).to eq(expected)
   end
 end
